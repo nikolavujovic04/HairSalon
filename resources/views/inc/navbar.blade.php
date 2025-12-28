60dbbb7538ebffdaad18a2129d796e5384340cdd
Nikola Vujovic <nikolavujovickv@gmail.com>
2025-12-28 22:12:48 +0100

    Kreiran navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="left-section">
+      <img src="./images/logo.png" alt="">
+    </div>
+
+    <div class="middle-section">
+      <a href="/home">Pocetna</a>
+      <a href="/about">O nama</a>
+      <a href="/services">Servisi</a>
+      <a href="/gallery">Galerija</a>
+      <a href="/contact">Kontakt</a>
+    </div>
+
+    <div class="right-section">
+      <button>
+        Contact
+      </button>
+    </div>
+</header>
